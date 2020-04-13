7d7147545d390635bcd518fcdad0bf15ee8d5bc9
Madhunika Akula <madhunika.akula@shrigowri.com>
2020-04-13 17:20:10 +0530

    middleware usage

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const http = require('http');
-const routes=require('./routes')
-console.log(routes.sometext);
-const server = http.createServer(routes.handler);
-server.listen(3000);
+const express = require('express');
+
+const app = express();
+
+app.use('/add',(req, res, next) => {
+    console.log("second middleware");
+    res.send("<h1>Express with add product</h1>")      // res.send will automatically set the header text/html
+});
+
+app.use('/', (req, res, next) => {
+    console.log("first middleware");
+    res.send("<h1>Express with no route</h1>")         
+    // next();                                         //next will allow to work with another middleware
+});
+
+// const server = http.createServer(app);
+
+app.listen(3000, () => {
+    console.log("server created")
+});

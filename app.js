86fbf413cf248782379be7374207bf7eccd147e7
Madhunika Akula <madhunika.akula@shrigowri.com>
2020-04-17 18:50:59 +0530

    registered all routes and made views

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs=require('express-handlebars');
-const path=require('path');
+
+const errorController = require('./controllers/error');
 
 const app = express();
-// app.engine('hbs',expressHbs({layoutsDir:'views/layouts/',defaultLayout:'main-layout',extname:'hbs'}));
-// app.set('view engine','hbs');//configuring handlebars to express  
-// app.set('view engine','pug'); 
- app.set('view engine','ejs');  
- app.set('views','views')//to know express about our views
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//serving files statically
-app.use(express.static(path.join(__dirname,'public')));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
+app.use(errorController.get404);
 
-const adminData=require('./routes/admin');
-const shoproutes=require('./routes/shop');
-
-//want to make our routes as per our url..
-app.use('/admin',adminData.routes);
-app.use(shoproutes);
-
-app.use((req,res,next)=>{
-    // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-    res.render('404',{pageTitle:'Page not found'}) //server side rendering html files.
-});
-
-app.listen(3300, () => {
-    console.log("server created");
+app.listen(3000,()=>{
+    console.log("started");
 });

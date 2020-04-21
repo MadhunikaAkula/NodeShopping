f4365f2ecb61c889c1c290859a3a2f9f7f548c95
Madhunika Akula <madhunika.akula@shrigowri.com>
2020-04-21 19:46:18 +0530

    sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,16 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'shopping-cart',
+//     password: 'admin'
+// });     
+//sql need to create pool to manage connections for each query.
+//sequelize object will automatically create pool on intialization .
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'shopping-cart',
-    password: 'admin'
-});
-
-module.exports = pool.promise();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-shopping', 'root', 'admin', {
+    dialect: 'mysql',
+    host: 'localhost'
+})
+module.exports = sequelize;
